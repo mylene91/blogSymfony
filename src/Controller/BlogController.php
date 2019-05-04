385f20ab94144eea896c8ab9b2b75eea87816ada
Myl91 <mylene.greneron@gmail.com>
2019-05-04 17:56:18 +0200

    ajout route et function show pour voir qu'un seul article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -27,4 +27,11 @@ class BlogController extends AbstractController
 
         ]);
     }
+
+    /**
+     * @Route("/blog/12", name="blog_show")
+     */
+    public function show() {
+        return $this->render('blog/show.html.twig');
+    }
 }
