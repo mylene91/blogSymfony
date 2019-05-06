17f3da754689d371bf2d700575fedc540d24db98
Myl91 <mylene.greneron@gmail.com>
2019-05-06 11:17:41 +0200

    injection de dépendances sur les méthodes indes() et show()

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,13 +12,14 @@ use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
+
+    // je veux dans la page /blog recup liste de mes articles et les donner à twig pour qu'il les affiche
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-
+        // je demande a doctrine de me donner un repository : Màj : plus besoin grâce à l'injection de dépendances
         // findOneByTitle('Titre de l\'article); <- trouvera par nom d'article si identique etc.
         $articles = $repo->findAll();
         return $this->render('blog/index.html.twig', [
@@ -26,8 +28,7 @@ class BlogController extends AbstractController
         ]);
     }
 
-    // je veux dans la page /blog recup liste de mes articles et les donner à twig pour qu'il les affiche
-    // repository des articles
+
 
     /**
      * @Route("/", name="home")
@@ -41,9 +42,15 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show() {
-        return $this->render('blog/show.html.twig');
+    public function show(ArticleRepository $repo, $id) {
+        //$repo = $this->getDoctrine()->getRepository((Article::class));
+        // trouve l'article qui a le même id qu'on m'a passé
+        $article = $repo->find($id);
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
